cc953ff57b62b720636cc97c81f0e91055b8dbb8
Christian Sabourin <christian.sabourin@canada.ca>
2023-05-04 16:47:14 -0400

    FIrst commit

---
+++ glossary.js
@@ -0,0 +1,185 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+	const docLang = document.documentElement.getAttribute("lang");
+    const dataFile = document.querySelector("[data-file]")?.getAttribute("data-file");
+	// Create a new style element and set its innerHTML to the provided CSS
+	const styleElement = document.createElement("style");
+	styleElement.innerHTML = `
+            .popup {
+                position: absolute;
+                z-index: 1;
+                background-color: #333;
+                color: #fff;
+                padding: 12px;
+                border-radius: 4px;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                max-width: 300px;
+                width: max-content;
+                display: none;
+            }
+    
+            .popup[aria-hidden="false"] {
+                display: block;
+            }
+    
+            .glossary {
+                position: relative;
+                text-decoration: underline dotted blue;
+                cursor: help;
+                color: #007bff;
+            }
+    
+            .glossary:hover {
+                color: #0056b3;
+            }
+    
+            .glossary:focus {
+                color: #0056b3;
+            }
+    
+            .tooltip-container {
+                display: inline;
+            }
+        `;
+
+	// Append the style element to the document head
+	document.head.appendChild(styleElement);
+	// Fetch the definitions from the JSON file
+	const url = dataFile ? dataFile : `glossary_${docLang}.txt`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((definitions) => {
+			// Loop through the definitions
+			definitions.forEach((definition) => {
+				// Find the glossary element with the matching term
+				const glossaryElement = Array.from(
+					document.querySelectorAll(".glossary")
+				).find(
+					(element) =>
+						element.getAttribute("data-term").trim().toLowerCase() ===
+						definition.term.trim().toLowerCase()
+				);
+
+				if (glossaryElement) {
+					glossaryElement.setAttribute("tabindex", "0");
+					// Create a new popup element
+					const popup = document.createElement("div");
+					popup.id = `popup_${definition.term}`;
+					popup.className = "popup";
+					popup.setAttribute("role", "tooltip");
+					popup.setAttribute("aria-hidden", "true");
+
+					// Check the document language and set the innerHTML accordingly
+					if (docLang && docLang.toLowerCase() === "fr") {
+						popup.innerHTML = `<p>Définition: ${definition.definition}</p>`;
+					} else {
+						popup.innerHTML = `<p>Definition: ${definition.definition}</p>`;
+					}
+
+					// Append the popup element to the glossary element
+					glossaryElement.appendChild(popup);
+
+					// Bind the tooltip events to the glossary element
+					bindTooltipEvents(glossaryElement, popup);
+				}
+			});
+		});
+
+	// The tooltip events code you provided
+	function bindTooltipEvents(tooltip, popup) {
+		let timeout;
+
+		tooltip.addEventListener("mouseenter", function (event) {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				positionPopup(event || window.event, tooltip, popup);
+				popup.setAttribute("aria-hidden", "false");
+			}, 500); // Wait half a second before displaying the tooltip
+		});
+
+		tooltip.addEventListener("mouseleave", function () {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				popup.setAttribute("aria-hidden", "true");
+			}, 350);
+		});
+
+		popup.addEventListener("mouseenter", function () {
+			clearTimeout(timeout);
+		});
+
+		popup.addEventListener("mouseleave", function () {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				popup.setAttribute("aria-hidden", "true");
+			}, 350);
+		});
+
+		tooltip.addEventListener("focus", function (event) {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				positionPopup(event || window.event, tooltip, popup);
+				popup.setAttribute("aria-hidden", "false");
+			}, 500); // Wait half a second before displaying the tooltip
+		});
+
+		tooltip.addEventListener("blur", function () {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				popup.setAttribute("aria-hidden", "true");
+			}, 350);
+		});
+
+		tooltip.addEventListener("touchstart", function (event) {
+			clearTimeout(timeout);
+			positionPopup(event || window.event, tooltip, popup);
+			if (popup.getAttribute("aria-hidden") === "true") {
+				popup.setAttribute("aria-hidden", "false");
+			} else {
+				popup.setAttribute("aria-hidden", "true");
+			}
+		});
+
+		function positionPopup(event, tooltip, popup) {
+			// Temporarily make the popup visible to calculate its dimensions
+			const originalDisplay = popup.style.display;
+			popup.style.visibility = "hidden";
+			popup.style.display = "block";
+
+			// Get the tooltip element's bounding rectangle
+			const tooltipRect = tooltip.getBoundingClientRect();
+
+			// Calculate the space above the tooltip element
+			const spaceAbove = tooltipRect.top;
+
+			// Calculate the space below the tooltip element
+			const spaceBelow = window.innerHeight - tooltipRect.bottom;
+
+			// Determine if there's enough space for the popup above the tooltip
+			const enoughSpaceAbove = spaceAbove > popup.offsetHeight + 8; // 16px equals 1rem
+
+			// Set the popup position based on the available space
+			popup.style.position = "fixed";
+
+			// Calculate left position while considering the 10 pixels margin
+			let leftPos = tooltipRect.left + window.scrollX;
+			if (leftPos < 20) {
+				leftPos = 20;
+			} else if (leftPos + popup.offsetWidth > window.innerWidth - 20) {
+				leftPos = window.innerWidth - 20 - popup.offsetWidth;
+			}
+			popup.style.left = `${leftPos}px`;
+
+			if (enoughSpaceAbove) {
+				popup.style.top = `${
+					tooltipRect.top - popup.offsetHeight - 8 + window.scrollY
+				}px`; // 16px equals 1rem
+			} else {
+				popup.style.top = `${tooltipRect.bottom + 8 + window.scrollY}px`; // 16px equals 1rem
+			}
+
+			// Restore the popup's original display value
+			popup.style.visibility = "visible";
+			popup.style.display = originalDisplay;
+		}
+	}
+});
---
+++ readme.md
@@ -0,0 +1,52 @@
+﻿# Glossary Tooltip Script
+
+This script creates an accessible tooltip for glossary terms with customizable definitions fetched from a JSON file. The tooltip appears above or below the glossary term based on the available space and includes a delay before displaying. The script also adapts to different document languages.
+
+## How to use
+
+1. Add the `.glossary` class to any element in your HTML that contains a glossary term.
+2. Add a `data-term` attribute to the `.glossary` element with the term as its value. This value will be used to fetch the correct definition from the JSON file.
+3. If you want to use a custom JSON file, add a `data-file` attribute to the script tag with the JSON file's path as its value.
+4. To display the tooltip in a specific language, set the `lang` attribute on the `<html>` element to the desired language code (e.g., "fr" for French).
+
+## Example usage
+
+```html
+<html lang="fr">
+  <head>
+    <!-- Add the script tag -->
+    <script src="path/to/tooltip-script.js" data-file="custom_glossary.json"></script>
+  </head>
+  <body>
+    <p>
+      This is an example sentence with a
+      <span class="glossary" data-term="example">glossary term</span>.
+    </p>
+  </body>
+</html>
+```
+
+## JSON file format
+
+The JSON file should contain an array of objects with the following structure:
+
+```json
+[
+  {
+    "term": "example",
+    "definition": "This is the definition of the example term."
+  },
+  {
+    "term": "another-term",
+    "definition": "This is the definition of another term."
+  }
+]
+```
+
+## Customization
+
+The script includes a CSS block that can be customized to style the tooltip, glossary terms, and tooltip container. To change the appearance, edit the `styleElement.innerHTML` string in the script.
+
+## Accessibility
+
+The script sets a `tabindex` attribute to every `.glossary` element, allowing keyboard navigation. It also includes the appropriate ARIA attributes for better screen reader support.
